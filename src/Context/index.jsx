7327731624fc33b49da2c3a351c2d6cb1171c2ea
Orangel Brito <orangeleando@gmail.com>
2023-06-21 17:15:11 -0500

    Mostrando productos en ProductDetail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,18 +3,27 @@ import { createContext, useState } from "react"
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
+	//ShoppingCart - Increment quantity
 	const [count, setCount] = useState(0)
-	const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
 
+	
+
+	//ProductDetail- Open/Close
+	const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
 	const openProductDetail = () => setIsProductDetailOpen(true)
 	const closeProductDetail = () => setIsProductDetailOpen(false)
+
+	//ProductDetail - Show Product
+	const [productToShow, setProductToShow] = useState({})
 	return (
 		<ShoppingCartContext.Provider value={{
 			count,
 			setCount,
 			openProductDetail,
 			closeProductDetail,
-			isProductDetailOpen
+			isProductDetailOpen,
+			productToShow,
+			setProductToShow
 		}}>
 			{children}
 		</ShoppingCartContext.Provider>
--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -9,9 +9,19 @@ const ProductDetail = () => {
 			<div className="flex justify-between items-center p-6">
 				<h2 className="font-medium text-xl">Detail</h2>
 				<div onClick={() => context.closeProductDetail()}>
-					<XMarkIcon className='h-6 w-6 text-black'/>
+					<XMarkIcon className='h-6 w-6 text-black cursor-pointer'/>
 				</div>
 			</div>
+			<figure className='px-6'>
+				<img className='w-full h-full rounded-lg '
+				src={context.productToShow.image} 
+				alt={context.productToShow.title} />
+			</figure>
+			<p className='flex flex-col p-6'>
+				<span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
+				<span className='font-medium text-md mb-1'>{context.productToShow.title}</span>
+				<span className='font-light text-sm'>{context.productToShow.description}</span>
+			</p>
 		</aside>
   )
 }
