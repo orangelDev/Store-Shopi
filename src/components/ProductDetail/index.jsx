4c9855c98fca32e7642fc3b3e812e6e84c9e0a6e
Orangel Brito <orangeleando@gmail.com>
2023-06-21 14:05:15 -0500

    Maquetando el ProductDetail

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const ProductDetail = () => {
+	const context = useContext(ShoppingCartContext)
   return (
-		<aside className="flex flex-col fixed right-0 border border-black rounded-lg bg-white w-[360px] h-[calc(100vh-80px)]">
+		<aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} flex flex-col fixed right-0 border border-black rounded-lg bg-white w-[360px] h-[calc(100vh-80px)]`}>
 			<div className="flex justify-between items-center p-6">
 				<h2 className="font-medium text-xl">Detail</h2>
-				<div>
+				<div onClick={() => context.closeProductDetail()}>
 					<XMarkIcon className='h-6 w-6 text-black'/>
 				</div>
 			</div>
