8ef7ce0785d112c8b99ec5a4533de3a6323b6150
Orangel Brito <orangeleando@gmail.com>
2023-06-23 12:58:35 -0500

    Eliminar y sumar  productos del carrito

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -2,9 +2,15 @@ import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import OrderCard from '../OrderCard'
+import { totalPrice } from '../../utils'
 
 const CheckoutSideMenu = () => {
 	const context = useContext(ShoppingCartContext)
+
+	const handleDelete = (id) => {
+		const filteredProducts = context.cartProducts.filter(product => product.id != id)
+		context.setCartProducts(filteredProducts)
+	}
 
   return (
 		<aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} flex flex-col fixed top-20 right-0 border border-black rounded-lg bg-white w-[360px] h-[calc(100vh-80px)]`}>
@@ -21,12 +27,20 @@ const CheckoutSideMenu = () => {
 					context.cartProducts.map(product => (
 						<OrderCard 
 							key={product.id}
+							id={product.id}
 							title={product.title}
 							imageUrl={product.image}
 							price={product.price}
+							handleDelete={handleDelete}
 						/>
 					))
 				}
+			</div>
+			<div className='px-6'>
+				<p className='flex justify-between items-center'>
+					<span className='font-light'>Total:</span>
+					<span className='font-medium text-1xl'>${totalPrice(context.cartProducts)}</span>
+				</p>
 			</div>
 		</aside>
   )
--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -1,7 +1,7 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = props => {
-	const {title, imageUrl, price } = props
+	const {id, title, imageUrl, price, handleDelete } = props
 	return (
 		<div className="flex justify-between items-center mb-3">
 			<div className="flex items-center gap-2">
@@ -13,6 +13,7 @@ const OrderCard = props => {
 			<div className="flex items-center gap-2">
 				<p className="text-md font-medium">{price}</p>
 				<XMarkIcon 
+						onClick={() => handleDelete(id)}
 						className='h-6 w-6 text-black cursor-pointer'/>
 			</div>
 		</div>
